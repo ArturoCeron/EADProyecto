2a75699c4e52b6f58fbfd9ab6abbda874652c77c
Arturo Ceron <43083944+ArturoCeron@users.noreply.github.com>
2021-05-28 14:09:57 -0700

    change port entry

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT && process.env.BIND_IP;
 
 //Motor de vistas EJS
 app.set('view engine', 'ejs');
